6b42206d5da40b09e8668472b5140fb17eb0a89f
laetuz <laetuzg@gmail.com>
2026-03-07 19:40:23 +0700

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,16 @@
+package id.neotica
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureMonitoring()
+    configureFrameworks()
+    configureDatabases()
+    configureRouting()
+}
